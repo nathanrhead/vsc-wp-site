3c5118840a1c2dba917093aaf0b4ab695b010566
Nathan Cox <nrc4@hotmail.com>
2025-06-06 13:20:45 -0700

    Add a books.php template to the child theme.

---
+++ books.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Books Page
+ * Pages Template
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+Responsive\responsive_wrapper_top(); // Before wrapper content hook.
+
+if (
+  ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ) &&
+  ! ( function_exists( 'rea_theme_template_render_at_location' ) && rea_theme_template_render_at_location( 'single' ) ) ) {
+
+  Responsive\responsive_wrapper();
+
+  while ( have_posts() ) :
+    the_post();
+    // get_template_part( 'partials/single/layout', 'book' );
+    the_content();
+  endwhile;
+
+  get_template_part( 'loop-nav', get_post_type() );
+  ?>
+    </main><!-- end of #primary -->
+  <?php
+
+  Responsive\responsive_wrapper_close();
+}
+
+Responsive\responsive_wrapper_end(); // After wrapper hook.
+get_footer();
